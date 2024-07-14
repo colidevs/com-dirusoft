de5157f2550fab4c5aa657212f62b98a85ed5dd1
Popri01 <lucas.popri@gmail.com>
2024-07-14 15:21:49 -0300

    Update faqs.tsx

--- src/components/faqs.tsx
+++ src/components/faqs.tsx
@@ -7,36 +7,37 @@ import {
 
 const faqs = [
   {
+    id: 0,
     question: "What is the logo design process?",
     answer: "Yes. It adheres to the WAI-ARIA design pattern.",
   },
-  {question: "How much does a logo design cost", answer: "None"},
-  {question: "How long does it take to design a logo", answer: "None"},
-  {question: "Do you offer custom logo design or use templates?", answer: "None"},
-  {question: "Can you redesign my existing logo?", answer: "None"},
-  {question: "What if I don't like the initial concepts?", answer: "None"},
-  {question: "How do I get started?", answer: "None"},
-  {question: "What payment methods do you accept?", answer: "None"},
+  {id: 1, question: "How much does a logo design cost", answer: "None"},
+  {id: 2, question: "How long does it take to design a logo", answer: "None"},
+  {id: 3, question: "Do you offer custom logo design or use templates?", answer: "None"},
+  {id: 4, question: "Can you redesign my existing logo?", answer: "None"},
+  {id: 5, question: "What if I don't like the initial concepts?", answer: "None"},
+  {id: 6, question: "How do I get started?", answer: "None"},
+  {id: 7, question: "What payment methods do you accept?", answer: "None"},
 ];
-
-let keys = 0;
 
 export default function Faqs() {
   return (
-    <article className="flex flex-col justify-center">
-      <header className="m-2 flex-col text-center text-white">
-        <h1 className=" m-2 text-8xl font-semibold">Our Faqs</h1>
-        <p className="text-xl text-gray-100/70">The most common question we get asked</p>
+    <article className="flex flex-col">
+      <header className="flex flex-col text-center  text-white">
+        <h1 className="text-8xl font-semibold leading-relaxed">Our Faqs</h1>
+        <p className=" text-xl text-gray-100/70">The most common question we get asked</p>
       </header>
-      <section className="flex">
-        <Accordion collapsible className=" mx-auto bg-black text-white" type="single">
-          {faqs.map((fq, index) => {
-            keys++;
-
+      <section className="flex justify-center ">
+        <Accordion collapsible className="w-full max-w-3xl flex-col  p-4 text-white " type="single">
+          {faqs.map((fq) => {
             return (
-              <AccordionItem key={keys} value={`item-${index}`}>
-                <AccordionTrigger>{fq.question}</AccordionTrigger>
-                <AccordionContent>{fq.answer}</AccordionContent>
+              <AccordionItem key={fq.id} className="mb-5" value={`item-${fq.id}`}>
+                <AccordionTrigger className=" rounded bg-slate-700 p-2 text-lg font-bold hover:text-yellow-300 hover:no-underline ">
+                  {fq.question}
+                </AccordionTrigger>
+                <AccordionContent className=" bg-gray-800 p-3 text-gray-100/70">
+                  {fq.answer}
+                </AccordionContent>
               </AccordionItem>
             );
           })}
